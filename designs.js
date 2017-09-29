9dcb578a89e326a06d5a552ebdb43114b8dc50b3
kerri alison <kerrivarner@gmail.com>
2017-09-28 21:50:21 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -2,34 +2,29 @@ const tbl = $("#pixel_canvas");
 
 // Function that clears previous grid and builds new grid based on inputs
 
-$('#sizePicker').submit(function(event) {
-            event.preventDefault();
-            //create table
-            let tbl = document.getElementById("pixel_canvas");
-            //clear previously created grid before building new grid
-            tbl.innerHTML = "";
-            //take value input for width and height
-            let height = $("#input_height").val();
-            let width = $("#input_width").val();
-            //iterate over values and add them to width and height
-            //this is not adding more rows/cells
-            for (let i = 0; i < height; i++) {
-                let row = tbl.insertRow();
+$("#sizePicker").submit(function(event) {
+  event.preventDefault();
+  //create table
+  let tbl = document.getElementById("pixel_canvas");
+  //clear previously created grid before building new grid
+  tbl.innerHTML = "";
+  //take value input for width and height
+  let height = $("#input_height").val();
+  let width = $("#input_width").val();
+  //iterate over values and add them to width and height
+  //this is not adding more rows/cells
+  for (let i = 0; i < height; i++) {
+    let row = tbl.insertRow();
 
-                let j = 0;
+    let j = 0;
 
-                while (j < width) {
-                    let cell = row.insertCell();
-                    cell.onclick(function() {
-                                //color isn't changing
-                            const changeColour = cell => {
-                                const colour = document.getElementById("colorPicker").value;
-
-                                cell.style.backgroundColor = colour;
-                            };
-                        }
-                    );
-            }
-            j++;
-        }
-       });
+    while (j < width) {
+      let cell = row.insertCell();
+      cell.onclick = function() { // reformatted onclick function
+        let colour = document.getElementById("colorPicker").value;
+        cell.style.backgroundColor = colour;
+      };
+    j++;
+    }
+  }
+});
